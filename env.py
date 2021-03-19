726c00c86b4a0cc5bf87ff7abb76a2de48c760f9
Denis Onder <denis_onder@protonmail.com>
2021-03-19 02:50:38 +0100

    Integrated the Spotify API to play songs (Premium required)

--- env.py
+++ env.py
@@ -4,6 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# --- OPENWEATHER ENVIRONMENTAL VARIABLES --- #
 open_weather_api_key = os.getenv("OPEN_WEATHER_API_KEY")
 city = os.getenv("CITY")
 weather_units = os.getenv("WEATHER_UNITS")
+
+# --- SPOTIFY ENVIRONMENTAL VARIABLES --- #
+spotify_api_scope = os.getenv("SPOTIFY_API_SCOPE")
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+spotify_redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
+spotify_username = os.getenv("SPOTIFY_USERNAME")
+spotify_device_name = os.getenv("SPOTIFY_DEVICE_NAME")
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from gtts import gTTS
 import speech_recognition as sr
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+
 import os
 
 import weather
+import music
+import env
 
 r = sr.Recognizer()
 muted_testing = None
+
+auth_manager = SpotifyOAuth(client_id=env.spotify_client_id,
+                            client_secret=env.spotify_client_secret,
+                            redirect_uri=env.spotify_redirect_uri,
+                            scope=env.spotify_api_scope,
+                            username=env.spotify_username)
+
+spotify = Spotify(auth_manager=auth_manager)
+
+devices = spotify.devices()
+
+device_id = None
+for d in devices['devices']:
+    d['name'] = d['name'].replace('’', '\'')
+    if d['name'] == env.spotify_device_name:
+        device_id = d['id']
+        break
 
 
 def create_sound_file(text, file_name):
@@ -30,12 +52,21 @@ def handle_speech(result):
     text = result.lower().strip()
 
     if text.__contains__("orchid"):
-        play_sound_file("hello")
+        hello = create_sound_file("Hello", "hello")
+        play_sound_file(hello)
 
     elif text.__contains__("tell me the weather"):
+        notifier = create_sound_file("Getting the weather data.",
+                                     "weather_notifier")
+        play_sound_file(notifier)
         weather_report = weather.get_weather_data()
         report_file = create_sound_file(weather_report, "weather_report")
         play_sound_file(report_file)
+
+    elif text.__contains__("play"):
+        command_fragments = text.split("play ")
+        track_uri = music.get_track_uri(command_fragments[1])
+        music.play_track(track_uri)
 
 
 def handle_speech_input():
@@ -54,10 +85,10 @@ def handle_speech_input():
 
 
 if __name__ == "__main__":
-    muted_testing = True  # or False if not testing manually
+    muted_testing = False  # or False if not testing manually
 
     if muted_testing:
         # !MUTED TESTING
-        handle_speech("tell me the weather")
+        handle_speech("play")
     else:
         handle_speech_input()
